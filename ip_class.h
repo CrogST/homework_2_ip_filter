ccb3971531f29708c1bba38990e1370093daac50
igor <samanovis@energomera.ru>
2019-11-09 22:05:42 +0300

    * порядок

--- ip_class.h
+++ ip_class.h
@@ -1,30 +1,14 @@
 #pragma once
 
-#include <string>
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include <iostream>
 
 using ip_t = std::vector<int>;
 using ip_list = std::vector<ip_t>;
-using ip_list_iter = ip_list::iterator;
-using filter_res = std::pair<ip_list_iter, ip_list_iter>;
+using filter_res = std::pair<ip_list::iterator, ip_list::iterator>;
 
-#define log_enable false
-
-class ip_class {
-
-#if log_enable
-static auto ip_out(ip_t val) {
-    std::string str;
-   for(const auto & el : val)
-       str += std::to_string(el) + " ";
-   return str;
-}
-#endif
-
-public:    
+struct ip_class {
 
     template <typename ...Args>
     static auto filter(ip_list & list, Args... args) {
@@ -32,38 +16,18 @@ public:
         ip_t ip_upper = ip_low;
         ip_low.insert(  ip_low.end(),   static_cast<size_t>(4 - ip_low.size()),   0);
         ip_upper.insert(ip_upper.end(), static_cast<size_t>(4 - ip_upper.size()), 255);
-
         //ищем первый ip, который меньше либо равен искомому
-        auto low_iter = std::lower_bound(list.begin(), list.end(), ip_upper,
-                                         [](const auto & it, const auto &val) {
-            auto cond = val < it;
-            if constexpr(log_enable) std::cout << "it " << ip_out(it) << ", val " << ip_out(val) <<
-                         "cond is " << ( cond ? "true" : "false") << std::endl;
-            return cond;
-        });
-        if constexpr(log_enable) std::cout << "low is " << ip_out(*low_iter) << std::endl;
-
+        auto low_iter = std::lower_bound(list.begin(), list.end(), ip_upper, std::greater<ip_t>());
         //ищем первый ip, который меньше искомого
-        auto upper_iter = std::upper_bound(low_iter, list.end(), ip_low,
-                                           [](const auto & val, const auto & it) {
-            auto cond = it < val;
-            if constexpr(log_enable) std::cout << "it " << ip_out(it) << ", val " << ip_out(val) <<
-                         "cond is " << ( cond ? "true" : "false") << std::endl;
-            return cond;
-        });
-        if constexpr(log_enable) std::cout << "upper is " << ip_out(*upper_iter) << std::endl;
+        auto upper_iter = std::upper_bound(low_iter, list.end(), ip_low, std::greater<ip_t>());
         return filter_res(low_iter, upper_iter);
     }
 
     static auto filter_any(ip_list list, int val) {
         ip_list list_out;
-        auto cmp_oct = [val](int oct) {
-            return (oct == val);
-        };
-        auto cmp_ip = [&cmp_oct](ip_t ip)
-        {
-            return std::any_of(ip.begin(), ip.end(), cmp_oct);
-        };
+        auto cmp_oct = [val](int oct) { return (oct == val); };
+        auto cmp_ip = [&cmp_oct](ip_t ip) {
+            return std::any_of(ip.begin(), ip.end(), cmp_oct); };
         std::copy_if(list.begin(), list.end(), std::back_inserter(list_out), cmp_ip);
         return list_out;
     }
--- ip_filter.cpp
+++ ip_filter.cpp
@@ -7,8 +7,6 @@
 
 #include "ip_class.h"
 
-static const bool debug = false;
-
 auto split(const std::string &str, char delim)
 {
     std::vector<std::string> r;
@@ -34,31 +32,16 @@ int main()
     {
         ip_list ip_pool;
 
-        if constexpr(!debug) {
-            //чтение файла построчно
-            //формирование вектора ip адресов.
-            for(std::string line; std::getline(std::cin, line);)
-            {
-                auto v = split(line, '\t');
-                auto ip_str = split(v.at(0), '.');
-                ip_t ip;
-                for(const auto & s : ip_str)
-                    ip.push_back(std::stoi(s));
-                ip_pool.push_back(ip);
-            }
-        } else { //отладка
-            ip_pool = {
-                {1,255,255,255},
-                {1,0,0,0},
-                {46,70,2,1},
-                {46,3,46,2},
-                {1,1,1,46},
-                {46,70,4,46},
-                {46,70,0,0},
-                {46,70,255,255},
-                {1,1,3,3},
-                {2,3,3,1}
-            };
+        //чтение файла построчно
+        //формирование вектора ip адресов.
+        for(std::string line; std::getline(std::cin, line);)
+        {
+            auto v = split(line, '\t');
+            auto ip_str = split(v.at(0), '.');
+            ip_t ip;
+            for(const auto & s : ip_str)
+                ip.push_back(std::stoi(s));
+            ip_pool.push_back(ip);
         }
 
         //вывод списка адресов
@@ -79,15 +62,12 @@ int main()
         std::sort(ip_pool.begin(), ip_pool.end(), std::greater<ip_t>());
         ip_list_out(ip_pool.begin(), ip_pool.end());
 
-        if(debug) std::cout << std::endl << "filter 1:" << std::endl;
         auto res = ip_class::filter(ip_pool, 1);
         ip_list_out(std::get<0>(res), std::get<1>(res));
 
-        if(debug) std::cout << std::endl << "filter 2:" << std::endl;
         res = ip_class::filter(ip_pool, 46, 70);
         ip_list_out(std::get<0>(res), std::get<1>(res));
 
-        if(debug) std::cout << std::endl << "filter 3:" << std::endl;
         auto res_any = ip_class::filter_any(ip_pool, 46);
         ip_list_out(res_any.begin(), res_any.end());
     }
